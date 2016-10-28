4b8f55a4c5ef17343473312990b398fc673e612a
Lagzder <Lvalek4@gmail.com>
2016-10-28 14:57:22 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,30 +9,10 @@ namespace Piskvorky
     class Program
     {
         static void Main(string[] args)
-        {
-
-            
+        {           
             Game mriezka = new Game();
             mriezka.VytvorMriezku();
-            /*for(int i = 0; i < 10; i++)
-            {
-                
-                if((poc % 2) == 0)
-                {
-                    Console.WriteLine("parne");
-                }
-                else Console.WriteLine("neparne");
-
-                poc = poc + 1;
-               
-                
-            }
-
-            /*for(int j = 0; j < 10; j++)
-            {
-               
-            }*/
-            
+              
             Console.ReadLine();
         }
     }
